47f3dc0ac379bae9809acb62636cfaef9236107e
Dakota710537 <dakotafernandez1025@gmail.com>
2025-08-27 12:07:56 -0700

    Add files via upload

--- reverseAstring.cpp
+++ reverseAstring.cpp
@@ -1,50 +1,49 @@
-//reverseAstring.cpp
-//dF 8/25/25
-
 #include <iostream>
 #include <string>
 #include <algorithm>
 #include <cctype>
 using namespace std;
-
-
+string trim(const string& strIn) {
+    string trimmed_s = strIn;
+    // Trim leading whitespace
+    trimmed_s.erase(trimmed_s.begin(), std::find_if(trimmed_s.begin(),
+    trimmed_s.end(), [](unsigned char ch) {
+    return !std::isspace(ch);
+    }));
+    // Trim trailing whitespace
+    trimmed_s.erase(std::find_if(trimmed_s.rbegin(), trimmed_s.rend(), [](unsigned
+    char ch) {
+    return !std::isspace(ch);
+    }).base(), trimmed_s.end());
+    return trimmed_s;
+}
 int main() {
-    cout << "\n\n Welcome to My String Reverse program!\n\n";
-
-string myStrToReverse = "A long String to Reverse";
-      cout << "\n The string before being reversed is: " << myStrToReverse;
-reverse( myStrToReverse.begin(), myStrToReverse.end() );
-     cout << "\n My reversed String is: " << myStrToReverse ;
-    
-
-    //pt 2
-    //Use a for loop to swap characters
-    for( int i = 0; i < 10; i++) {
-        cout << "\n The value of the i is: " << i << "\n";
-    }
-
-
-    //Create a string object
-    string myStr01 = "   this is a literal string   ";
-
-    //Find the length method
+    cout << "\n\n Welcome to My Reverse a String program!\n\n";
+    // Part 1
+    // Use the reverse() function from the library named algoritthm
+    // Create a string that we will reverse
+    string myStrToReverse = "A Long String to Reverse";
+    cout << "\n The string before being reversed is: " << myStrToReverse;
+    reverse(myStrToReverse.begin(), myStrToReverse.end());
+    cout << "\n My reversed string is: " << myStrToReverse;
+    // Part 2
+    // Various string methods
+    // Create a string object
+    string myStr01 = " this is a literal string ";
+    // Find the length method
     int strlength = 0;
-
     strlength = myStr01.length();
     // Output the length of the string:
-    cout << "\n The length of myStr01 is: " << strlength;
-
-
-//output the string objects value
-cout << "\n\n myStr01 is: " << myStr01 << "\n\n";
-
+    cout << "\n The length of my myStr01 is: " << strlength;
+    // Output the string object's value.
+    cout << "\n\n myStr01 is:" << myStr01 << "\n";
+    // trim the empty spaces
     string myTrimmedStr = trim(myStr01);
-    //find the string objects length
-    cout << "\n Length is: " << myTrimmedStr.length() << "\n\n";
-
-    cout << "\n The trim string is: " << myTrimmedStr << "\n\n";
-
-
-
+    // find the string object's length
+    strlength = myTrimmedStr.length();
+    // output the length of the string:
+    cout << "\n The length of my myTrimmedStr is: " << strlength;
+    // Output the trimmed string:
+    cout << "\n myTrimmedStr is:" << myTrimmedStr << "\n\n";
     return 0;
 }
