61acf22984116d126c2e55351b9f77925b72ee63
Dakota710537 <dakotafernandez1025@gmail.com>
2025-08-27 12:02:19 -0700

    Add files via upload

---
+++ reverseString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include
+using namespace std;
+
+int main() {
+
+    string test1 = "abcdefghi";
+
+    cout << test1 << endl;
+
+    cout << "size: " << test1.size() << endl;
+
+    return 0;
+}
