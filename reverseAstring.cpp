aad76042ee01066e060faf06ec41c56b703401cc
Dakota710537 <dakotafernandez1025@gmail.com>
2025-08-27 12:04:44 -0700

    Add files via upload

--- reverseAstring.cpp
+++ reverseAstring.cpp
@@ -4,7 +4,9 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cctype>
 using namespace std;
+
 
 int main() {
     cout << "\n\n Welcome to My String Reverse program!\n\n";
@@ -20,6 +22,29 @@ reverse( myStrToReverse.begin(), myStrToReverse.end() );
     for( int i = 0; i < 10; i++) {
         cout << "\n The value of the i is: " << i << "\n";
     }
+
+
+    //Create a string object
+    string myStr01 = "   this is a literal string   ";
+
+    //Find the length method
+    int strlength = 0;
+
+    strlength = myStr01.length();
+    // Output the length of the string:
+    cout << "\n The length of myStr01 is: " << strlength;
+
+
+//output the string objects value
+cout << "\n\n myStr01 is: " << myStr01 << "\n\n";
+
+    string myTrimmedStr = trim(myStr01);
+    //find the string objects length
+    cout << "\n Length is: " << myTrimmedStr.length() << "\n\n";
+
+    cout << "\n The trim string is: " << myTrimmedStr << "\n\n";
+
+
 
     return 0;
 }
